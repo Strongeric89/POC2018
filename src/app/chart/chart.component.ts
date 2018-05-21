1fd90656eb57dae1e5bafd7849aa06d8b0ba74c9
Eric Strong <eric.strong@sap.com>
2018-05-21 13:22:11 +0100

    dynamic with new json_sample - works

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -60,67 +60,87 @@ export class ChartComponent implements OnInit {
     public nodes: Array<any> = [];
    
     ngOnInit() {
-     
         this.dataservice.getAll2().subscribe(data=>{
-                    //get number of cols needed
-                    var number_of_cols_lift = data['lift']['cols'].length;
-                    var number_of_cols_gain = data['gain']['cols'].length;
-                    var number_of_cols_cm = data['cm']['cols'].length;
-                    var number_of_cols_sample = data['sample_data_1']['cols'].length;
-                
-                    //Lift Chart
-                    this.chartLabels = this.addPercent(data['lift']['data'][0]); // this is the chartLabels (x- axis)
-                
-                    for(var x=1; x< number_of_cols_lift; x++){
-                       
-                        //create new object
-                        var obj = {data: data['lift']['data'][x], label:data['lift']['cols'][x]};
+            var number_of_cols_lift = data['debrief']['Lift']['rows'].length;
+            var number_of_cols_gain = data['debrief']['Gain']['rows'].length;
+            var number_of_cols_cm =  data['debrief']['Confusion_Matrix']['columes'].length;
+            //var number_of_cols_sample =  data['debrief']['sample']['src']['columes'].length;
+            console.log(number_of_cols_lift)
 
-                        //get random css for that dataset
-                        this.chartColors.push(this.createCSSForChart(x));
-                       //load the chartData sets with new dynamic data
-                        this.chartDatasets.push(obj);
+            //get first element x from each array
+            var colBase = 0;
+            for(var x = 0; x < number_of_cols_lift; x++){
+               
+                this.chartLabels.push(data['debrief']['Lift']['rows'][x][0] + '%');
+                this.dataObj(colBase,number_of_cols_lift,data,'Lift');
+                colBase++;
+              
+            }
 
-                    }
-                
-                    if(this.chartLabels.length > 0){
-                        this.chartFlag = true;
-                    }
-
-                    //Gain Chart
-                
-                    this.chartLabels2 = this.addPercent(data['gain']['data'][0]); // this is the chartLabels (x- axis)
-                
-                    for(var x=1; x< number_of_cols_gain; x++){
-                       
-                        //create new object
-                        var obj = {data: data['gain']['data'][x], label:data['gain']['cols'][x]};
-
-                        //get random css for that dataset
-                        this.chartColors2.push(this.createCSSForChart(x));
-                       //load the chartData sets with new dynamic data
-                        this.chartDatasets2.push(obj);
-
-                    }
-                
-                    if(this.chartLabels2.length > 0){
-                        this.chartFlag2 = true;
-                    }
+            colBase = 0;
+            for(var x = 0; x < number_of_cols_gain; x++){
+               
+                this.chartLabels2.push(data['debrief']['Gain']['rows'][x][0] + '%');
+                this.dataObj(colBase,number_of_cols_gain,data,'Gain');
+                colBase++;
+              
+            }
 
 
-        })//end foreach 
+            //console.log(this.chartLabels)
+
+            if(this.chartLabels.length > 0){
+                this.chartFlag = true;
+            }
+
+            if(this.chartLabels2.length > 0){
+                this.chartFlag2 = true;
+            }
+           
+
+        });//end 
+     
+    
   
     }//end ngOnInit
 
+    public dataObj(colBase, number_of_cols,data,chartName){
+        var d = [];
+        var y = 0;
+        
+        for(var x = 0; x < number_of_cols; x++){
+            //get data for object
+            d.push(data['debrief'][chartName]['rows'][x][colBase])
+            
+              
+        }
 
-    public addPercent(labels){
-        var newLabels = [];
-        labels.forEach((item) =>{
-            newLabels.push(item + '%');
-        });
-        return newLabels;
+        //create object     
+        var obj = {data:d, label:data['debrief'][chartName]['columes'][colBase]};
+        console.log(obj)
+        //add object to chart data 
+        if(obj.label == 'Index' || obj.label == null){
+            return;
+        }
+
+        if(chartName == 'Lift'){
+            this.chartDatasets.push(obj);
+            //create the css for each
+          this.chartColors.push(this.createCSSForChart(colBase + 1));
+        }
+
+        if(chartName == 'Gain'){
+            this.chartDatasets2.push(obj);
+            //create the css for each
+          this.chartColors2.push(this.createCSSForChart(colBase + 1));
+        }
+
+        
+      
+      
 
     }
+
 
     public createCSSForChart(index){
 
@@ -128,7 +148,6 @@ export class ChartComponent implements OnInit {
 
         //var randomColor = colors[Math.floor(Math.random()*colors.length)];
         var color = colors[index];
-       
 
         var cssObj = {
         backgroundColor: 'transparent',
@@ -141,14 +160,70 @@ export class ChartComponent implements OnInit {
         };
 
         //logic to prevent color from getting picked again
+        //console.log(cssObj);
 
         return cssObj;
 
 
     }
 
-    
+
+//     public initializeCharts(){
+//         this.dataservice.getAll2().subscribe(data=>{
+//             //get number of cols needed
+
+          
+
+          
+//             var number_of_cols_lift = data['debrief']['Lift']['rows'].length;
+//             var number_of_cols_gain = data['gain']['cols'].length;
+//             var number_of_cols_cm = data['cm']['cols'].length;
+//             var number_of_cols_sample = data['sample_data_1']['cols'].length;
+        
+//             //Lift Chart
+//            // this.chartLabels = this.addPercent(data['lift']['data'][0]); // this is the chartLabels (x- axis)
+        
+//             for(var x=1; x< number_of_cols_lift; x++){
+               
+//                 //create new object
+//                 var obj = {data: data['lift']['data'][x], label:data['lift']['cols'][x]};
+
+//                 //get random css for that dataset
+//                 this.chartColors.push(this.createCSSForChart(x));
+//                //load the chartData sets with new dynamic data
+//                 this.chartDatasets.push(obj);
+
+//             }
+        
+//             if(this.chartLabels.length > 0){
+//                 this.chartFlag = true;
+//             }
+
+//             //Gain Chart
+        
+//             this.chartLabels2 = this.addPercent(data['gain']['data'][0]); // this is the chartLabels (x- axis)
+        
+//             for(var x=1; x< number_of_cols_gain; x++){
+               
+//                 //create new object
+//                 var obj = {data: data['gain']['data'][x], label:data['gain']['cols'][x]};
+
+//                 //get random css for that dataset
+//                 this.chartColors2.push(this.createCSSForChart(x));
+//                //load the chartData sets with new dynamic data
+//                 this.chartDatasets2.push(obj);
+
+//             }
+        
+//             if(this.chartLabels2.length > 0){
+//                 this.chartFlag2 = true;
+//             }
 
 
+// })//end foreach 
+
+
+
+//     }
 
 }
