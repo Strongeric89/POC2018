b776aa164562842a852c9e0889758ca73b22c9b5
Eric Strong <eric.strong@sap.com>
2018-05-21 13:57:03 +0100

    added changes over to mlChart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../services/data.service';
+import { DataService } from '../services/data.service';
 
 @Component({
     selector: 'app-chart',
@@ -7,6 +7,8 @@ import {DataService} from '../services/data.service';
     styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
+
+    public numofcols = 0; // for css to select different colors
 
 
     // Lift chart
@@ -30,133 +32,127 @@ export class ChartComponent implements OnInit {
     }
 
 
-    //Gain chart
-     // Lift chart
-     public chartFlag2 = false;
-     public chartType2: string = 'line';
- 
-     public chartDatasets2: Array<any> = [];
-     public chartLabels2: Array<any> = [];
-     public chartColors2: Array<any> = [];
- 
-     public chartOptions2: any = {
-         responsive: true
-     };
- 
-     public chartClicked2(e: any): void {
- 
-     }
- 
-     public chartHovered2(e: any): void {
- 
-     }
- 
+    // Lift chart
+    public chartFlag2 = false;
+    public chartType2: string = 'line';
+
+    public chartDatasets2: Array<any> = [];
+    public chartLabels2: Array<any> = [];
+    public chartColors2: Array<any> = [];
+
+    public chartOptions2: any = {
+        responsive: true
+    };
+
+    public chartClicked2(e: any): void {
+
+    }
+
+    public chartHovered2(e: any): void {
+
+    }
+
 
 
     constructor(private dataservice: DataService) { }
-    
-    json: string[];
- 
-    public nodes: Array<any> = [];
-   
+
     ngOnInit() {
-        this.dataservice.getAll2().subscribe(data=>{
+        this.dataservice.getAll2().subscribe(data => {
             var number_of_cols_lift = data['debrief']['Lift']['rows'].length;
             var number_of_cols_gain = data['debrief']['Gain']['rows'].length;
-            var number_of_cols_cm =  data['debrief']['Confusion_Matrix']['columes'].length;
+            var number_of_cols_cm = data['debrief']['Confusion_Matrix']['columes'].length;
+            this.numofcols =  data['debrief']['Lift']['columes'].length;
+           
             //var number_of_cols_sample =  data['debrief']['sample']['src']['columes'].length;
-            console.log(number_of_cols_lift)
+        
 
             //get first element x from each array
             var colBase = 0;
-            for(var x = 0; x < number_of_cols_lift; x++){
-               
+            for (var x = 0; x < number_of_cols_lift; x++) {
+
                 this.chartLabels.push(data['debrief']['Lift']['rows'][x][0] + '%');
-                this.dataObj(colBase,number_of_cols_lift,data,'Lift');
+                this.dataObj(colBase, number_of_cols_lift, data, 'Lift');
                 colBase++;
-              
+
             }
 
             colBase = 0;
-            for(var x = 0; x < number_of_cols_gain; x++){
-               
+            for (var x = 0; x < number_of_cols_gain; x++) {
+
                 this.chartLabels2.push(data['debrief']['Gain']['rows'][x][0] + '%');
-                this.dataObj(colBase,number_of_cols_gain,data,'Gain');
+                this.dataObj(colBase, number_of_cols_gain, data, 'Gain');
                 colBase++;
-              
+
             }
 
 
             //console.log(this.chartLabels)
 
-            if(this.chartLabels.length > 0){
+            if (this.chartLabels.length > 0) {
                 this.chartFlag = true;
             }
 
-            if(this.chartLabels2.length > 0){
+            if (this.chartLabels2.length > 0) {
                 this.chartFlag2 = true;
             }
-           
+
 
         });//end 
-     
-    
-  
+
+
+
     }//end ngOnInit
 
-    public dataObj(colBase, number_of_cols,data,chartName){
-        var d = [];
-        var y = 0;
-        
-        for(var x = 0; x < number_of_cols; x++){
+    public dataObj(colBase, number_of_cols, data, chartName) {
+        var d = []; // array for data
+
+        for (var x = 0; x < number_of_cols; x++) {
             //get data for object
             d.push(data['debrief'][chartName]['rows'][x][colBase])
-            
-              
+
+
         }
 
         //create object     
-        var obj = {data:d, label:data['debrief'][chartName]['columes'][colBase]};
+        var obj = { data: d, label: data['debrief'][chartName]['columes'][colBase] };
         console.log(obj)
         //add object to chart data 
-        if(obj.label == 'Index' || obj.label == null){
+        if (obj.label == 'Index' || obj.label == null) {
             return;
         }
 
-        if(chartName == 'Lift'){
+        if (chartName == 'Lift') {
             this.chartDatasets.push(obj);
             //create the css for each
-          this.chartColors.push(this.createCSSForChart(colBase + 1));
+            this.chartColors.push(this.createCSSForChart(colBase + 1)); // + 1 to not select empty index
         }
 
-        if(chartName == 'Gain'){
+        if (chartName == 'Gain') {
             this.chartDatasets2.push(obj);
             //create the css for each
-          this.chartColors2.push(this.createCSSForChart(colBase + 1));
+            this.chartColors2.push(this.createCSSForChart(colBase + this.numofcols )); // + 3 to select different colors
         }
 
-        
-      
-      
+
 
     }
 
 
-    public createCSSForChart(index){
+    public createCSSForChart(index) {
 
-        var colors = [' ','red','green','blue','purple','yellow','orange','pink','brown','grey'];
+        var colors = [' ', 'red', 'green', 'blue', 'purple', 'yellow', 'orange', 'brown', 'yellow', 'grey'];
 
         //var randomColor = colors[Math.floor(Math.random()*colors.length)];
         var color = colors[index];
 
         var cssObj = {
-        backgroundColor: 'transparent',
-        borderColor: color ,
-        borderWidth: 6,
-        pointBackgroundColor: color,
-        pointBorderColor: color,
-        pointHoverBackgroundColor: color,
-        pointHoverBorderColor: color
+            backgroundColor: 'transparent',
+            borderColor: color,
+            borderWidth: 6,
+            pointBackgroundColor: color,
+            pointBorderColor: color,
+            pointHoverBackgroundColor: color,
+            pointHoverBorderColor: color
         };
 
         //logic to prevent color from getting picked again
@@ -168,62 +164,62 @@ export class ChartComponent implements OnInit {
     }
 
 
-//     public initializeCharts(){
-//         this.dataservice.getAll2().subscribe(data=>{
-//             //get number of cols needed
-
-          
-
-          
-//             var number_of_cols_lift = data['debrief']['Lift']['rows'].length;
-//             var number_of_cols_gain = data['gain']['cols'].length;
-//             var number_of_cols_cm = data['cm']['cols'].length;
-//             var number_of_cols_sample = data['sample_data_1']['cols'].length;
-        
-//             //Lift Chart
-//            // this.chartLabels = this.addPercent(data['lift']['data'][0]); // this is the chartLabels (x- axis)
-        
-//             for(var x=1; x< number_of_cols_lift; x++){
-               
-//                 //create new object
-//                 var obj = {data: data['lift']['data'][x], label:data['lift']['cols'][x]};
-
-//                 //get random css for that dataset
-//                 this.chartColors.push(this.createCSSForChart(x));
-//                //load the chartData sets with new dynamic data
-//                 this.chartDatasets.push(obj);
-
-//             }
-        
-//             if(this.chartLabels.length > 0){
-//                 this.chartFlag = true;
-//             }
-
-//             //Gain Chart
-        
-//             this.chartLabels2 = this.addPercent(data['gain']['data'][0]); // this is the chartLabels (x- axis)
-        
-//             for(var x=1; x< number_of_cols_gain; x++){
-               
-//                 //create new object
-//                 var obj = {data: data['gain']['data'][x], label:data['gain']['cols'][x]};
-
-//                 //get random css for that dataset
-//                 this.chartColors2.push(this.createCSSForChart(x));
-//                //load the chartData sets with new dynamic data
-//                 this.chartDatasets2.push(obj);
-
-//             }
-        
-//             if(this.chartLabels2.length > 0){
-//                 this.chartFlag2 = true;
-//             }
-
-
-// })//end foreach 
+    //     public initializeCharts(){
+    //         this.dataservice.getAll2().subscribe(data=>{
+    //             //get number of cols needed
 
 
 
-//     }
+
+    //             var number_of_cols_lift = data['debrief']['Lift']['rows'].length;
+    //             var number_of_cols_gain = data['gain']['cols'].length;
+    //             var number_of_cols_cm = data['cm']['cols'].length;
+    //             var number_of_cols_sample = data['sample_data_1']['cols'].length;
+
+    //             //Lift Chart
+    //            // this.chartLabels = this.addPercent(data['lift']['data'][0]); // this is the chartLabels (x- axis)
+
+    //             for(var x=1; x< number_of_cols_lift; x++){
+
+    //                 //create new object
+    //                 var obj = {data: data['lift']['data'][x], label:data['lift']['cols'][x]};
+
+    //                 //get random css for that dataset
+    //                 this.chartColors.push(this.createCSSForChart(x));
+    //                //load the chartData sets with new dynamic data
+    //                 this.chartDatasets.push(obj);
+
+    //             }
+
+    //             if(this.chartLabels.length > 0){
+    //                 this.chartFlag = true;
+    //             }
+
+    //             //Gain Chart
+
+    //             this.chartLabels2 = this.addPercent(data['gain']['data'][0]); // this is the chartLabels (x- axis)
+
+    //             for(var x=1; x< number_of_cols_gain; x++){
+
+    //                 //create new object
+    //                 var obj = {data: data['gain']['data'][x], label:data['gain']['cols'][x]};
+
+    //                 //get random css for that dataset
+    //                 this.chartColors2.push(this.createCSSForChart(x));
+    //                //load the chartData sets with new dynamic data
+    //                 this.chartDatasets2.push(obj);
+
+    //             }
+
+    //             if(this.chartLabels2.length > 0){
+    //                 this.chartFlag2 = true;
+    //             }
+
+
+    // })//end foreach 
+
+
+
+    //     }
 
 }
